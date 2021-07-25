f3cb0b82c057af0896c0e5ce0e6d6518b617a7ed
Swati Singh <singhswati.13055@gmail.com>
2021-07-25 21:39:52 +0200

    data structure lists, tuple and set

---
+++ lists_indexing.py
@@ -0,0 +1,6 @@
+arr = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+print(arr[0])
+print(arr[3:])
+print(arr[:3])
+print(arr[2:6])
+print(arr[4:])
---
+++ tuple_add_pop.py
@@ -0,0 +1,8 @@
+## using add and pop function 
+
+a = [1, 2, 2, 3, 3, 3, 4, 4, 4, 4]
+b = set(a)
+b.add(5)
+print(b)
+b.pop()
+print(b)
