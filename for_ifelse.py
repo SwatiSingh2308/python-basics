cd225ef94846a089e46d858614c08348742d0700
Swati Singh <singhswati.13055@gmail.com>
2021-07-30 22:08:12 +0200

    for loop

---
+++ for_ifelse.py
@@ -0,0 +1,6 @@
+for i in range(31):# Write a for loop using range() to print out multiples of 5 up to 30 inclusive
+  if i%5==0:
+      print(i) 
+  else:
+          print("number not multiple of 5")
+
---
+++ for_loop.py
@@ -0,0 +1,4 @@
+sentence = ["the", "quick", "brown", "fox", "jumped", "over", "the", "lazy", "dog"]
+
+for word in range(len(sentence)):# Write a for loop to print out each word in the sentence list, one word per line
+  print(sentence[word])
